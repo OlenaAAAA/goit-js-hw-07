bd792ec6e337c913633dd95cb220d4fbbe42a36a
OlenaAAAA <130498592+OlenaAAAA@users.noreply.github.com>
2023-08-20 15:12:05 +0300

    2nd task is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector(".gallery");
+const renderList = (arr, container) => {
+    const markup = arr.map(({ preview, original, description }) => 
+        
+    `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+     <img
+      class="gallery__image"
+      width="1280"
+      src="${preview}"
+      alt="${description}"
+      />
+     </a>
+    </li>`).join("");
+
+container.insertAdjacentHTML("beforeend", markup);
+};
+
+renderList(galleryItems, galleryContainer)
+console.log(galleryContainer);
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+    captionDelay: 250,
+    });
+
